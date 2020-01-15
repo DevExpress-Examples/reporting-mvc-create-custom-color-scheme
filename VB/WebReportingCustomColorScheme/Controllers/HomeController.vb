3503fac6f45b35a2eee76c64152e1b103f66fdac
DevExpressExampleBot <fakeEmail>
2020-01-15 17:22:04 +0300

    autogenerate VB

---
+++ VB/WebReportingCustomColorScheme/Controllers/HomeController.vb
@@ -0,0 +1,22 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+
+Namespace WebReportingCustomColorScheme.Controllers
+	Public Class HomeController
+		Inherits Controller
+
+		Public Function Index() As ActionResult
+			Return View()
+		End Function
+
+		Public Function Designer() As ActionResult
+			Return View()
+		End Function
+
+		Public Function Viewer() As ActionResult
+			Return View()
+		End Function
+	End Class
+End Namespace
---
+++ VB/WebReportingCustomColorScheme/PredefinedReports/ReportsFactory.vb
@@ -0,0 +1,13 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Threading.Tasks
+
+Namespace WebReportingCustomColorScheme.PredefinedReports
+	Public Module ReportsFactory
+		Sub New()
+			Reports.Add("TestReport", Function() New TestReport())
+		End Sub
+		Public Reports As New Dictionary(Of String, Func(Of XtraReport))()
+	End Module
+End Namespace
